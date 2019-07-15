4021b97121d7157d519599060760d00f62f8bae5
juztinlazaro <juztinlazaro@gmail.com>
2019-07-15 18:10:55 +0800

    changes: update unit test

--- src/components/SearchCityResult/SearchCityResult.test.tsx
+++ src/components/SearchCityResult/SearchCityResult.test.tsx
@@ -5,22 +5,33 @@ import EnzymeAdapter from 'enzyme-adapter-react-16';
 import sinon from 'sinon';
 
 import { FAKE_LOCATIONS } from 'common/fakeData';
+import EmptySlate from 'components/EmptySlate';
+
 import SearchCityResult from './index';
 
 configure({ adapter: new EnzymeAdapter() });
 
 describe('LETS TEST TABLE BOX COMPONENT', () => {
-  let SearchCityResultWrapper: any;
-
   const onOpenModal = sinon.spy();
 
-  beforeEach(() => {
-    SearchCityResultWrapper = shallow(
+  it('should render 11 location item', () => {
+    const SearchCityResultWrapper = shallow(
       <SearchCityResult onOpenModal={onOpenModal} locations={FAKE_LOCATIONS} />,
     );
+    expect(SearchCityResultWrapper.find('.item')).toHaveLength(11);
   });
 
-  it('should render 11 location item', () => {
-    expect(SearchCityResultWrapper.find('.item')).toHaveLength(11);
+  it('should not render location item', () => {
+    const SearchCityResultWrapper = shallow(
+      <SearchCityResult onOpenModal={onOpenModal} locations={[]} />,
+    );
+    expect(SearchCityResultWrapper.find('.item')).toHaveLength(0);
+  });
+
+  it('should not render EmptySlate', () => {
+    const SearchCityResultWrapper = shallow(
+      <SearchCityResult onOpenModal={onOpenModal} locations={[]} />,
+    );
+    expect(SearchCityResultWrapper.find(EmptySlate)).toHaveLength(1);
   });
 });
